923e8708efbc26bb9e94ca64127abcc23df5abf7
michellhornung <michellhornung@c8e9faed-83ac-47b6-ad74-6541724da2ee>
2017-09-09 01:14:38 +0000

    Backend for STOCK is done. Going to implement the VIEW.

---
+++ branches/roadie-studio-1.0.0/src/main/java/com/hornung/roadiestudio/repository/StockType.java
@@ -0,0 +1,7 @@
+package com.hornung.roadiestudio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockType extends JpaRepository<com.hornung.roadiestudio.model.StockType, Integer>{
+
+}
